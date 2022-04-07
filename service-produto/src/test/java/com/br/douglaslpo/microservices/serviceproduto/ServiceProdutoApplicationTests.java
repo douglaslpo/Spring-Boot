d80690ae11ff37da5bd313f34262480065753aba
DouglasLPO <douglaslpo@bol.com.br>
2022-04-07 19:27:00 -0300

    first commit

---
+++ service-produto/src/test/java/com/br/douglaslpo/microservices/serviceproduto/ServiceProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.douglaslpo.microservices.serviceproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
